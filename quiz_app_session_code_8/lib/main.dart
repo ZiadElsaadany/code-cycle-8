51503c743dee7ed9423af84be37cd074675479ad
ziad elsaadany <ziadelsaadany73@gmail.com>
2024-04-02 16:58:46 +0200

    initial commit

---
+++ quiz_app_session_code_8/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'features/home/views/home_screen.dart';
+
+void main( )   {
+
+  runApp(QuizApp());
+}
+
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+
